8d17c402fdabad66e7b4a42294d70f2514d0451c
shunta <exec.apea@gmail.com>
2019-12-20 21:46:38 +0700

    UPDATE: settings.py

--- src/project/project/settings.py
+++ src/project/project/settings.py
@@ -18,10 +18,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 dotenv_path = join(BASE_DIR, '.env')
 load_dotenv(dotenv_path)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -34,7 +32,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -50,6 +47,7 @@ INSTALLED_APPS = [
     # third apps
     'bootstrap4',
     'crispy_forms',
+    'django_cleanup',
 
 ]
 
@@ -81,12 +79,9 @@ TEMPLATES = [
     },
 ]
 
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
 WSGI_APPLICATION = 'project.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -104,7 +99,6 @@ DATABASES = {
         },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -124,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -138,7 +131,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -148,7 +140,6 @@ STATIC_URL = '/static/'  # 本番環境で使われるurl
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 
 # Custom Useer
 AUTH_USER_MODEL = 'sns.User'
